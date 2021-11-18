5ec7b5dfa163b078708e77153ca1a7c859bf8e78
Gabriel <1123120054@qq.com>
2021-11-18 20:03:39 +0800

    2-2 init

---
+++ src/main/java/com/jiawa/wiki/WikiApplication.java
@@ -0,0 +1,16 @@
+package com.jiawa.wiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Jiat
+ */
+@SpringBootApplication
+public class WikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikiApplication.class, args);
+    }
+
+}
