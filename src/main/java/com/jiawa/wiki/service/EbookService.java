480ceab8a7a8a3dba8ac91a33cedb92940a75ba3
Gabriel <1123120054@qq.com>
2021-11-19 15:33:05 +0800

    3-7 制作CopyUtil封装BeanUtils

--- src/main/java/com/jiawa/wiki/service/EbookService.java
+++ src/main/java/com/jiawa/wiki/service/EbookService.java
@@ -5,6 +5,7 @@ import com.jiawa.wiki.domain.EbookExample;
 import com.jiawa.wiki.mapper.EbookMapper;
 import com.jiawa.wiki.req.EbookReq;
 import com.jiawa.wiki.resp.EbookResp;
+import com.jiawa.wiki.util.CopyUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -22,12 +23,16 @@ public class EbookService {
         criteria.andNameLike("%"+req.getName()+"%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook:ebookList){
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook,ebookResp);
-            respList.add(ebookResp);
-        }
-        return respList;
+//        List<EbookResp> respList = new ArrayList<>();
+//        for (Ebook ebook:ebookList){
+////           EbookResp ebookResp = new EbookResp();
+////           BeanUtils.copyProperties(ebook,ebookResp);
+//            //对象复制
+//            EbookResp ebookResp=CopyUtil.copy(ebook,EbookResp.class);
+//            respList.add(ebookResp);
+//        }
+        //列表复制
+        List<EbookResp> list =CopyUtil.copyList(ebookList,EbookResp.class);
+        return list;
     }
 }
