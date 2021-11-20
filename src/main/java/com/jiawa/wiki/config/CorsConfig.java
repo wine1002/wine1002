e9e72841e1fb76a4a1872636bec20095394b2c30
Gabriel <1123120054@qq.com>
2021-11-20 14:20:14 +0800

    3-10 首页路由开发

---
+++ src/main/java/com/jiawa/wiki/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.jiawa.wiki.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Jiat
+ * 解决跨域连接问题
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedHeaders(CorsConfiguration.ALL)
+                .allowedMethods(CorsConfiguration.ALL)
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
